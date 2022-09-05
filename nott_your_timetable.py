1ac68346a09a5f564c91b88b0ea827b2fbe5a8a0
ecyht2 <ecyht2@nottingham.edu.my>
2022-09-05 11:39:23 +0800

    Improved Error handling

--- nott_your_timetable.py
+++ nott_your_timetable.py
@@ -5,14 +5,20 @@ import argparse
 
 
 def main_cli(args: argparse.Namespace):
-    days = handle_ranges(args.days)
-    weeks = handle_ranges(args.weeks)
+    try:
+        days = handle_ranges(args.days)
+        weeks = handle_ranges(args.weeks)
+    except ValueError:
+        print("Invalid Range, Please Check Inserted Value")
+        return 1
     output = args.output
 
     if days[0] < 1 or days[-1] > 7:
-        raise ValueError("Invalid Range, Please Check Inserted Value")
+        print("Invalid Range, Please Check Inserted Value")
+        return 1
     elif weeks[0] < 1 or weeks[-1] > 52:
-        raise ValueError("Invalid Range, Please Check Inserted Value")
+        print("Invalid Range, Please Check Inserted Value")
+        return 1
 
     program = "UG/M1024/M6UEEENG/F/02"
 
@@ -32,3 +38,4 @@ height=100&week=100"
         data[table] = table_to_dict(tables[table], verbose=False)
 
     csv_export(data, weeks, output)
+    return 0
